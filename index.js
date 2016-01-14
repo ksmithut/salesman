1bbf24dd8eea4022c1e596e09c3cd186074a9042
ksmithut <ksmith.ut@gmail.com>
2016-01-14 13:48:30 -0700

    renamed to salesman

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
+var Schema = require('./lib/schema');
+var Salesman = require('./lib/salesman');
 
+module.exports = exports = new Salesman();
+
+exports.Schema = Schema;
+exports.Salesman = Salesman;
